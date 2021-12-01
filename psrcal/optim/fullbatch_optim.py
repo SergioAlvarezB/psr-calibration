4039f35ce0c6b4ad4cdba35cd5eef96018da900d
bsxfan <bsxfan@gmail.com>
2021-12-01 06:20:53 +0200

    adding some optimization code

---
+++ psrcal/optim/fullbatch_optim.py
@@ -0,0 +1,65 @@
+from scipy.optimize import minimize 
+
+
+
+class Callback:
+    """
+    A small tool for tracking iterations. Not of direct concern to the user.
+    
+    Objects of this class can be passed as callback to scipy.optimize.minimize().
+    """
+    def __init__(self):
+        self.iter = 0
+        self.scores = []
+    def __call__(self,*p):
+        self.iter += 1
+    def record_score(self,score):
+        scores, iter = self.scores, self.iter
+        if iter < len(scores):
+            scores[iter] = score
+        else:
+            scores.append(score)
+
+
+
+
+    
+    
+def train_obj_scipy_lbfgs(obj, paramvec0, maxiters = 20, quiet=False):
+    """
+    Minimizes the given objective using scipy L-BFGS-B. 
+    
+    Inputs:
+        obj: maps paramvec --> loss, gradient  (input and outputs are numpy)
+        paramvec0: a numpy vector to initialize the optimnization
+    
+    Returns: 
+        
+        - the final paramvec 
+        - the final minimum objective value 
+        - a list of objective values per iteration (the training curve)
+        
+    """
+    
+    cb = Callback()
+    
+    def obj_cb(paramvec):
+        loss_np, grad_np = obj(paramvec)
+        cb.record_score(loss_np)
+        if not quiet: print("iter",cb.iter,": ",loss_np)
+        return loss_np, grad_np
+
+    res = minimize(obj_cb, paramvec0, method="L-BFGS-B", jac=True, callback=cb, 
+                   options={'maxiter':maxiters}) 
+    if not quiet:
+        print("scipy lbfgs termination status:")
+        print(" ",res.message)
+        print("  success:",res.success)
+        print("  obj:",res.fun)
+        print("  niters:",res.nit)
+        print("  nfun, ngrad:",res.nfev, res.njev)
+        print()
+        
+    return res.x, res.fun, cb.scores
+
+    
---
+++ psrcal/optim/vecmodule.py
@@ -0,0 +1,97 @@
+from torch.nn import Module, Parameter
+from torch.nn.utils import vector_to_parameters, parameters_to_vector
+from torch import as_tensor as ten
+
+from psrcal.optim.fullbatch_optim import train_obj_scipy_lbfgs
+
+class VecModule(Module):
+    """
+    This subclass of torch.nn.Module provides a convenient interface to a 
+    vectorized view of all the trainable parameters in the module. The 
+    trainable parameters are those that have requires_grad=True.
+    """
+    
+    def __init__(self):
+        super().__init__()
+        self.trainmode = True
+    
+    def trainable_parameters(self): 
+        """
+        This is a generator (i.e. iterator), that returns all of the trainable
+        parameters in this module, and recursively in all contained modules.
+        
+        If you want all parameters instead, use self.parameters(), as in any
+        Module.
+        """
+        for p in self.parameters(): 
+            if p.requires_grad: yield p
+
+    def set_paramvec(self, paramvec, zero_grad = False):
+        """
+        Writes the given numpy vector into the trainable parameters
+        """
+        vector_to_parameters(ten(paramvec),self.trainable_parameters())
+        if zero_grad: self.zero_grad()
+
+    def get_paramvec(self):
+        """
+        Extracts the trainable parameters as a numpy vector
+        """
+        return parameters_to_vector(self.trainable_parameters()).detach().numpy()
+    
+    def get_gradvec(self):
+        """
+        Extracts the current accumulated gradient of the trainable parameters 
+        as a numpy vector
+        """
+        grads = (p.grad for p in self.trainable_parameters())
+        return parameters_to_vector(grads).detach().numpy()
+
+
+    def set_evalmode(self):
+        """
+        Stores requires_grad for each parameter into self.flags, so that we
+        can later revert to trainmode again. Then sets requires_grad = False
+        for all parmeters and sets grad=None for all parameters.
+        """
+        if not self.trainmode: return
+        self.flags = [p.requires_grad for p in self.parameters()]    
+        for p in self.parameters(): 
+            p.requires_grad = False
+            p.grad = None
+
+    def set_trainmode(self):
+        """
+        Reverts to trainmode by recovering requires_grad for each parmeter
+        from the stored flags. Sets grad=None for each parameter.
+        """
+        if self.trainmode: return
+        for p, flag in zip(self.parameters(), self.flags): 
+            p.requires_grad = flag
+            p.grad = None
+            
+            
+class LBFGS_Objective(VecModule):
+    """
+    This is a Module and a VecModule. You should implement a subclass to serve
+    as LBFGS optimization objective. You should implement a method called
+    loss, with signature loss(self) that returns the objective value. Gradient 
+    will be done automatically by torch. To retrieve a vector of parameters,
+    use yourmodule.get_params(). The to-vbe-optimized optimized params include 
+    all members wrapped as Parameter and all Params in members that are also 
+    Modules.
+    
+    """
+    def __init__(self):
+        super().__init__()            
+        
+    def __call__(self,paramvec):
+        self.set_paramvec(paramvec,True)
+        loss = self.loss()
+        loss.backward()
+        return loss.item(), self.get_gradvec()
+    
+    
+def lbfgs(obj,maxiters,quiet=False):    
+        return train_obj_scipy_lbfgs(obj,obj.get_paramvec(),maxiters,quiet)
+        
